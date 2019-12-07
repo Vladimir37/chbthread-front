02affa9945b825afb21d3706583a04b7ce23ecf9
Vladimir37 <vladimir37work@gmail.com>
2019-12-08 02:40:45 +0300

    Axios timeout

--- src/utils/provider.js
+++ src/utils/provider.js
@@ -3,7 +3,7 @@ import config from '../config';
 
 export default axios.create({
     baseURL: config.apiUrl,
-    timeout: 1000,
+    timeout: 30000,
     headers: {'Access-Control-Allow-Credentials': true},
     withCredentials: true,
     crossDomain: true,
